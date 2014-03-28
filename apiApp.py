907bbbbfc0f99f605c11ebcbb357297323e7c654
yuchixing <void.yu@hotmail.com>
2014-03-28 18:05:58 +0800

    aaa

--- apiApp.py
+++ apiApp.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 
-from flask import Flask
+from flask import Flask,redirect,url_for,render_template
 from flask.ext.restful import reqparse, abort, Api, Resource
 
 from flask.ext import admin
@@ -8,12 +8,40 @@ from flask.ext.mongoengine import MongoEngine
 from flask.ext.admin.form import rules
 from flask.ext.admin.contrib.mongoengine import ModelView
 
+import auth
+
+import json
+
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='FWAPIAPP#$'
-app.config['MONGODB_SETTINGS']={'DB':'testDB','HOST':'192.168.100.77','PORT':27017}
+app.config['MONGODB_SETTINGS']={'DB':'testDB','HOST':'192.168.100.142','PORT':27017}
 
 api = Api(app)
+
+
+#############################db-Model######################
+db=MongoEngine()
+db.init_app(app)
+
+class  AppAdmin(db.Document):
+	"""docstring for  AppAdmin"""
+	name=db.StringField(max_length=20)
+	appToken=db.StringField(max_length=60)
+
+	def __unicode__(self):
+		return self.name
+		
+class CaptchaAdmin(db.Document):
+	phone=db.StringField(max_length=11)
+	phoneMessNum=db.StringField(max_length=4)
+	#create_time=db.DateTimeField()
+	create_time=db.IntField()
+
+	def __unicode__(self):
+		return self.phone
 
 #input arg
 #
@@ -21,9 +49,22 @@ parser = reqparse.RequestParser()
 parser.add_argument('appName', type=str)
 parser.add_argument('appToken', type=str)
 
+parser.add_argument('phoneNum', type=str)
+parser.add_argument('stamp', type=str)
 def getRandomText():
-	#oath2
-	pass
+	import random
+	num="0123456789"
+	phoneNum=''.join(random.sample(num,4))
+	return phoneNum
+
+def datetime2stamp(utcnow):
+	import time
+	stamp=int(time.mktime(utcnow.utctimetuple()))
+	return stamp
+
+
+def stamp2datetime(stamp):
+	parse_args
 
 def checkRandomText():
 	pass
@@ -37,10 +78,25 @@ class Captcha(Resource):
 	def post(self,optName):
 		print "optName===",optName
 		if optName=='get':
-			return {'a':'a'}
+			import datetime
+			paramDict=parser.parse_args()
+			#save db
+			now=datetime.datetime.utcnow()
+			ca=CaptchaAdmin()
+			ca.phone=paramDict['phoneNum']
+			ca.phoneMessNum=getRandomText()
+			ca.create_time=datetime2stamp(now)
+			ca.save()
+
+			#send message to phone
+			return {'code':'1','stamp':ca.create_time}
+
 		elif optName=='check':
-			print 'post==',dir(self)
-			print parser.parse_args()
+			#check stamp and phoneNum
+			paramDict=parser.parse_args()
+			if paramDict['phoneNum'] and paramDict['stamp']:
+				print dir(Captcha)
+				#print Captcha.object.get({'phone':paramDict['phoneNum'],'create_time':paramDict['stamp']})
 			return {'e':'e'}
 		else:
 			return {'code':-1,'msg':u'error message'}
@@ -50,18 +106,27 @@ api.add_resource(Captcha, '/<string:optName>')
 
 
 
-#############################admin######################
-db=MongoEngine()
-db.init_app(app)
+###########################admin############
 
-class  AppAdmin(db.Document):
-	"""docstring for  AppAdmin"""
-	def __init__(self, arg):
-		super( AppAdmin, self).__init__()
-		self.arg = arg
-		
+class APIAdminView(auth.AuthModelView):
+	form_widget_args={
+		'app_url':{'style':'width:500px;'},
+		'app_description':{'rows':4,'style':'width:500px;'}
+	}
+
+
+
+@app.route('/')
+@auth.requires_auth
+def index():
+	return render_template('index.html')
+	#return redirect(url_for('apiadminview.admin'))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	admin=admin.Admin(app,u'API')
+	admin.add_view(APIAdminView(AppAdmin,name=u'app'))
+	admin.add_view(APIAdminView(CaptchaAdmin,name=u'captcha'))
+	app.run(debug=True)
 
 
